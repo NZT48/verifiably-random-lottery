626b12fd7713886c6dcb2b0373b550b8f91663a8
NZT48 <nikolaztodorovic26@gmail.com>
2025-01-12 13:23:15 +0100

    Implement enter raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,9 +10,12 @@ pragma solidity ^0.8.18;
  * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
  */
 contract Raffle {
-    uint256 private immutable i_entranceFee;
-
     error Raffle_NotEnoughEthSent();
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -20,7 +23,8 @@ contract Raffle {
 
     function enterRaffle() external payable {
         if(msg.value < i_entranceFee) revert Raffle_NotEnoughEthSent();
-
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {
